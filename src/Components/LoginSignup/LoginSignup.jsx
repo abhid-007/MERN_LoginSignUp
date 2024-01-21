28ad04392630f0bb5e9ae310b8f4669e87042e5e
Abhisek Das <abhid.cs@outlook.com>
2024-01-21 22:42:27 +0530

    second commit

---
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './LoginSignup.css'
+
+
+import user_icon from '../Assets/person.png'
+import email_icon from'../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
+
+const LoginSignup = () => {
+    return (
+        <div className='container'>
+            <div className="header">
+            <div className='text'>SignUp</div>
+            <div className='underline'>Login</div>
+        
+        </div>
+        </div>
+    )}
+
+export default LoginSignup
